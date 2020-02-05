15a67f4d25755d2051bde6ec926ad73f445fab54
liweiwh <liweiwh@itcast.cn>
2020-02-05 09:56:02 +0800

    修改正式请求的后端地址

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export default {
   baseUrl: {
     dev: 'http://localhost:3000',
-    pro: 'http://dev.itcastor.com:11000'
+    pro: 'http://dev.itcastor.com:10080'
   },
   publicPath: [/^\/public/, /^\/login/]
 }
